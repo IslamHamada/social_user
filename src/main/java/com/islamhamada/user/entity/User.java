33801d9ce551a73914052ff91edbb48c067ab14f
IslamHamada <islamhamada222@gmail.com>
2026-03-02 05:55:19 +0100

    add basic User table

---
+++ src/main/java/com/islamhamada/user/entity/User.java
@@ -0,0 +1,19 @@
+package com.islamhamada.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String username;
+    String email;
+}

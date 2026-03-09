534a030b9ef5d61e71dc00b16ae57d9aa6bc1b24
IslamHamada <islamhamada222@gmail.com>
2026-03-09 21:26:44 +0100

    add registerUser function and implemenetation

--- src/main/java/com/islamhamada/user/service/UserService.java
+++ src/main/java/com/islamhamada/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.islamhamada.user.service;
 
 import com.islamhamada.social.dto.UserDTO;
+import com.islamhamada.user.model.RegisterUserRequest;
+
 import java.util.List;
 
 public interface UserService {
     List<UserDTO> getUsers();
+    void registerUser(RegisterUserRequest request);
 }
--- src/main/java/com/islamhamada/user/service/UserServiceImpl.java
+++ src/main/java/com/islamhamada/user/service/UserServiceImpl.java
@@ -1,6 +1,13 @@
 package com.islamhamada.user.service;
 
 import com.islamhamada.social.dto.UserDTO;
+import com.islamhamada.user.entity.User;
+import com.islamhamada.user.model.RegisterUserRequest;
+import com.islamhamada.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -21,5 +28,18 @@ public class UserServiceImpl implements UserService{
                         .build())
                 .toList();
         return usersDTOs;
+    }
+
+    @Override
+    public void registerUser(RegisterUserRequest request) {
+        Optional<User> user = userRepository.findByAuth0Id(request.getAuth0Id());
+        if(!user.isPresent()){
+            User user2 = User.builder()
+                            .username(request.getUsername())
+                            .email(request.getEmail())
+                            .auth0Id(request.getAuth0Id())
+                            .build();
+            userRepository.save(user2);
+        }
     }
 }

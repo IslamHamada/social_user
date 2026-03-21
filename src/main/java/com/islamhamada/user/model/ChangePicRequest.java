238c50951cb553c7ceba73bd6cb9fca6a85d0c2f
IslamHamada <islamhamada222@gmail.com>
2026-03-21 19:37:22 +0100

    add ChangePicRequest

---
+++ src/main/java/com/islamhamada/user/model/ChangePicRequest.java
@@ -0,0 +1,16 @@
+package com.islamhamada.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChangePicRequest {
+    String username;
+    MultipartFile pic;
+}
